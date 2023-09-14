1f390b915d2d3ca9a924388866d60b95423ed876
seaso <seasonmaharjan@kathmandu.codes>
2023-09-14 12:38:28 +0545

    2

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'loginpage',
       theme: ThemeData(
-
+        fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
 
       ),
--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -14,80 +14,83 @@ class HomeState extends State<MyHomePage>{
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: size.width,
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+    return
+      Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.access_time_filled_sharp,size: size.width/5,color: const Color(
-                  0xff550909),),
-              const Text("DEMO",
-                style: TextStyle(color: Colors.black,fontSize: 14,
-                    fontWeight: FontWeight.bold),)
-            ],
-          ),
-          Container(
-            height: size.height/2.5,
-            width: size.width/1.1,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(30)
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 20,),
-                 const Padding(padding: EdgeInsets.only(left: 25,top: 15),
-                     child: Text("Sign in",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),)),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                      height: 50,
-                      width: size.width/1.2,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20)
-                      ),
-                      child: const Center(child: Text("Email",style: TextStyle(color: Colors.brown,fontSize: 12),))
-                    ),
-                  ),
-                  Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                      height: 50,
-                      width: size.width/1.2,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20)
-                      ),
-                      child: const Center(child: Text("Password",style: TextStyle(color: Colors.brown,fontSize: 12),))
-                  ),
-                ),
-                  const SizedBox(height: 20,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 50,
-                        width: 120,
-                        decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadius.circular(20)
-                        ),
-                        child: const Center(child: Text("Signin",style: TextStyle(fontSize: 13,color: Colors.white), )),
-                      ),
-                    ],
-                  )
+                Icon(Icons.access_time_filled_sharp,size: size.width/5,color: const Color(
+                    0xff550909),),
+                const Text("DEMO",
+                  style: TextStyle(color: Colors.black,fontSize: 14,
+                      fontWeight: FontWeight.bold),)
               ],
             ),
-          )
-        ],
+            Container(
+              height: size.height/2.5,
+              width: size.width/1.1,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(30)
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20,),
+                   const Padding(padding: EdgeInsets.only(left: 25,top: 15),
+                       child: Text("Sign in",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),)),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        height: 50,
+                        width: size.width/1.2,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: const Center(child: Text("Email",style: TextStyle(color: Colors.brown,fontSize: 12),))
+                      ),
+                    ),
+                    Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                        height: 50,
+                        width: size.width/1.2,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: const Center(child: Text("Password",style: TextStyle(color: Colors.brown,fontSize: 12),))
+                    ),
+                  ),
+                    const SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: const Center(child: Text("Signin",style: TextStyle(fontSize: 13,color: Colors.white), )),
+                        ),
+                      ],
+                    )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
