8ae47210855f7b04af965823cc12f64dcd67acdb
Santosh Tamang <105838336+Xic00000@users.noreply.github.com>
2023-09-15 21:02:24 +0545

    "Container"

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:logindemo/secondPage.dart';
 
 class MyHomePage extends StatefulWidget{
   const MyHomePage({super.key});
@@ -16,82 +16,89 @@ class HomeState extends State<MyHomePage>{
     var size = MediaQuery.of(context).size;
     return
       Scaffold(
-      body: Container(
-        height: size.height,
-        width: size.width,
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+        body: Container(
+          height: size.height,
+          width: size.width,
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.access_time_filled_sharp,size: size.width/5,color: const Color(
-                    0xff550909),),
-                const Text("DEMO",
-                  style: TextStyle(color: Colors.black,fontSize: 14,
-                      fontWeight: FontWeight.bold),)
-              ],
-            ),
-            Container(
-              height: size.height/2.5,
-              width: size.width/1.1,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(30)
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const SizedBox(height: 20,),
-                   const Padding(padding: EdgeInsets.only(left: 25,top: 15),
-                       child: Text("Sign in",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),)),
+                  Icon(Icons.access_time_filled_sharp,size: size.width/5,color: const Color(
+                      0xff550909),),
+                  const Text("DEMO",
+                    style: TextStyle(color: Colors.black,fontSize: 14,
+                        fontWeight: FontWeight.bold),)
+                ],
+              ),
+              Container(
+                height: size.height/2.5,
+                width: size.width/1.1,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(30)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 20,),
+                    const Padding(padding: EdgeInsets.only(left: 25,top: 15),
+                        child: Text("Sign in",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),)),
                     Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: Container(
-                        height: 50,
-                        width: size.width/1.2,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20)
-                        ),
-                        child: const Center(child: Text("Email",style: TextStyle(color: Colors.brown,fontSize: 12),))
+                          height: 50,
+                          width: size.width/1.2,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: const Center(child: Text("Email",style: TextStyle(color: Colors.brown,fontSize: 12),))
                       ),
                     ),
                     Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                        height: 50,
-                        width: size.width/1.2,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20)
-                        ),
-                        child: const Center(child: Text("Password",style: TextStyle(color: Colors.brown,fontSize: 12),))
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                          height: 50,
+                          width: size.width/1.2,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: const Center(child: Text("Password",style: TextStyle(color: Colors.brown,fontSize: 12),))
+                      ),
                     ),
-                  ),
                     const SizedBox(height: 20,),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Container(
-                          height: 50,
-                          width: 120,
-                          decoration: BoxDecoration(
-                            color: Colors.orange,
-                            borderRadius: BorderRadius.circular(20)
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.of(context).push(
+                                MaterialPageRoute(builder: (context) =>  SecondPage())
+                            );
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 120,
+                            decoration: BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: const Center(child: Text("Signin",style: TextStyle(fontSize: 13,color: Colors.white), )),
                           ),
-                          child: const Center(child: Text("Signin",style: TextStyle(fontSize: 13,color: Colors.white), )),
                         ),
                       ],
                     )
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
