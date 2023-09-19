542964f9bc4592ad550633ee56a94370d0bb67e0
Santosh Tamang <105838336+Xic00000@users.noreply.github.com>
2023-09-19 17:01:39 +0545

    ListView

--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -11,13 +11,132 @@ class SecondPage extends StatefulWidget{
 }
 
 class secondpageState extends State<SecondPage>{
+
+  showContainer(BuildContext context, int orderid){
+    var size = MediaQuery.of(context).size;
+    return Container(
+      //  height: size.height/4,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: size.width/1.1,
+            height: 50,
+            padding: const EdgeInsets.only(left: 18, right: 20),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child:  Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:  [
+                Text("Order Id :$orderid" , style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
+                const Text("Accepted", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
+              ],
+            ),
+          ),
+          const SizedBox(height: 15,),
+          Container(
+            width: size.width/1.2,
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.check_box),
+                    Text("Order Note", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.check_box),
+                    Text("14:20 PM", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                    Text("Apr 18, 2022")
+                  ],
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 15,),
+          Container(
+            width: size.width/1.2,
+            child: const Text("More spicy and crispy chickened with extra olive oil and garlic with a hint of vinegar."
+                ,style: TextStyle(overflow: TextOverflow.visible),maxLines: 3),
+          ),
+
+          const SizedBox(height: 15,),
+          Container(
+            padding: const EdgeInsets.only(left:15),
+            // width: size.width/1.2,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.check_box),
+                        Text("No. of items : 5", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.check_box),
+                        Text("Created by : Random", style: TextStyle(fontWeight: FontWeight.bold),)
+                      ],
+                    ),
+                    //  const SizedBox(height: 8,)
+                  ],
+                ),
+                Container(
+                  width: size.width/3,
+                  decoration: const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20)
+                      )
+                  ),
+                  child:  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.check_box,color: Colors.white,),
+                          Text(" Total", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.currency_rupee_outlined,color: Colors.white,),
+                          Text(" 420.00", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(backgroundColor: Colors.transparent,),
-      body: Column(
+      body: ListView(
         children: [
           const Padding(
             padding: EdgeInsets.all(30.0),
@@ -27,104 +146,19 @@ class secondpageState extends State<SecondPage>{
               ],
             ),
           ),
-          Container(
-            height: size.height/4,
-            width: size.width/1.1,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  width: size.width/1.1,
-                  height: 50,
-                  padding: const EdgeInsets.only(left: 18, right: 20),
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Order Id : 123", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
-                      Text("Accepted", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 15,),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.check_box),
-                        Text("Order Note", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.check_box),
-                        Text("14:20 PM", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
-                        Text("Apr 18, 2022")
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(height: 15,),
-                const Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text("More spicy and crispy chickened with extra olive oil"),
-                    Text("and garlic with a hint of vinegar.")
-                  ],
-                ),
-                const SizedBox(height: 15,),
-                Row(
-                  children: [
-                    const Column(
-                      children: [
-                        Row(
-                          children: [
-                            Icon(Icons.check_box),
-                            Text("No. of items : 5", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.check_box),
-                            Text("Created by : Random", style: TextStyle(fontWeight: FontWeight.bold),)
-                          ],
-                        )
-                      ],
-                    ),
-                    Container(
-                      width: size.width/2,
-                      decoration: const BoxDecoration(
-                        color: Colors.grey
-                      ),
-                      child: const Column(
-                        children: [
-                          Row(
-                            children: [
-                              Icon(Icons.check_box,),
-                              Text("Total", style: TextStyle(fontWeight: FontWeight.bold),)
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.currency_rupee_outlined),
-                              Text("420.00", style: TextStyle(fontWeight: FontWeight.bold),)
-                            ],
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
+              showContainer(context, 200),
+              const SizedBox(height: 10,),
+              showContainer(context, 200),
+              const SizedBox(height: 10,),
+              showContainer(context, 200),
+              const SizedBox(height: 10,),
+              showContainer(context, 200),
+              const SizedBox(height: 10,),
+              showContainer(context, 200),
+              const SizedBox(height: 10,),
+              showContainer(context, 200),
+              const SizedBox(height: 10,),
+              showContainer(context, 3)
         ],
       ),
     );
